3255249472bb69201d1c35ad37782a0764751934
村上智紀 <tomomura0828@gmail.com>
2019-03-01 09:05:55 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,42 @@
+def hangman(word):
+    wrong = 0
+    stages = ["",
+              "          ",
+              "-----     ",
+              "|    |    ",
+              "|    o    ",
+              "|    |    ",
+              "|   \ /   ",
+              "|    |    ",
+              "|   / \   ",
+              "|         "
+              ]
+    rletters = list(word)
+    board = ["_"]*len(word)
+    win = False
+    print("Welcom to hangman")
+    while wrong < len(stages) -1:
+        print("\n")
+        msg = "Guess charactor"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = "$"
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("you win")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("you lose. The answer is {}.".format(word))
+
+hangman("tomonori")
+            
+        
